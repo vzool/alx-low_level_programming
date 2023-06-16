c6f8183a9373a1603fa7edc2826893f4c71d0662
root <>
2023-06-16 13:25:41 -0700

    9. Patience, persistence and perspiration make an unbeatable combination for success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	const char letters[] = "0, 1, 2, 3, 4, 5, 6, 7, 8, 9";
+	int i;
+
+	for (i = 0; i < 28; i++)
+	{
+		putchar(letters[i]);
+	}
+	putchar('\n');
+	return (0);
+}
