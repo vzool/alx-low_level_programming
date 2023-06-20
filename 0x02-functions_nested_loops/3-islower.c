a8f80a76625c72d58744dc5184c447e29bda7a61
root <>
2023-06-20 03:05:08 -0700

    task 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -12,22 +12,3 @@ int _islower(int c)
 	return (c >= 'a' && c <= 'z');
 }
 
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-	int r;
-
-	r = _islower('H');
-	_putchar(r + '0');
-	r = _islower('o');
-	_putchar(r + '0');
-	r = _islower(108);
-	_putchar(r + '0');
-	_putchar('\n');
-	return (0);
-}
-
