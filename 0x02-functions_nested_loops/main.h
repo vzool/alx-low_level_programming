55e1aa68f94783b35c40ba3594c44a712cf6134a
root <>
2023-06-20 02:44:45 -0700

    Task 1

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,17 +1,6 @@
 #ifndef HEADER_MAIN
 #define HEADER_MAIN
 
-#include <stdio.h>
-
-/**
- * _putchar - print a character
- * @c: character to print
- *
- * Return: int
- */
-int _putchar(char c)
-{
-	return putchar(c);
-}
+int _putchar(char c);
 
 #endif
