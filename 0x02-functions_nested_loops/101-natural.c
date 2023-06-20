085914820250f797ce2af5209ead18f80d2114fb
root <>
2023-06-20 06:42:07 -0700

    wip

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -7,5 +7,19 @@
  */
 int main(void)
 {
+	int i, j, x;
+
+	for (i = 0; i < 1024; i++)
+	{
+		x = 0;
+		for(j = 0; j <= i; j++)
+		{
+			if (j % 3 == 0 || j % 5 == 0 || j % 6 == 0)
+			{
+				x += j;
+			}
+		}
+		printf("%d\n", x);
+	}
 	return (0);
 }
