1cf1322af07891d7d7e4d7a05a9e58bc3954da30
root <>
2023-06-20 03:02:58 -0700

    Task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,5 +1,10 @@
 #include "main.h"
 
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet
+ *
+ * Return: void
+ */
 void print_alphabet_x10(void)
 {
 	int i;
@@ -13,13 +18,3 @@ void print_alphabet_x10(void)
 	}
 }
 
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-	print_alphabet_x10();
-	return (0);
-}
