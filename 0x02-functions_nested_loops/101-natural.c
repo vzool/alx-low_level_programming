5f2747505a32915c7b30fdf9e306c02790f13b64
root <>
2023-06-20 06:45:36 -0700

    Task 13

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -7,19 +7,13 @@
  */
 int main(void)
 {
-	int i, j, x;
+	int i, x = 0;
 
 	for (i = 0; i < 1024; i++)
 	{
-		x = 0;
-		for(j = 0; j <= i; j++)
-		{
-			if (j % 3 == 0 || j % 5 == 0 || j % 6 == 0)
-			{
-				x += j;
-			}
-		}
-		printf("%d\n", x);
+		if (i % 3 == 0 || i % 5 == 0 || i % 6 == 0)
+			x += i;
 	}
+	printf("%d\n", x);
 	return (0);
 }
