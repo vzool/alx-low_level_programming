4f3a574851de2b3078be23e7369dece9fc8a3ecb
root <>
2023-06-20 02:24:47 -0700

    Taks 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,15 +1,4 @@
-#include <stdio.h>
-
-/**
- * _putchar - print a character
- * @c: character to print
- *
- * Return: int
- */
-int _putchar(char c)
-{
-	return (putchar(c));
-}
+#include "main.h"
 
 /**
  * main - Entry point
