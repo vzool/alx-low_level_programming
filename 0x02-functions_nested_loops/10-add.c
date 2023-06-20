c1bd8c80b6427aa5cbbe5d1113a88aa30967352d
root <>
2023-06-20 03:46:56 -0700

    Task 10

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -13,17 +13,3 @@ int add(int a, int b)
 	return (a + b);
 }
 
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-	int n;
-
-	n = add(89, 9);
-	printf("%d\n", n);
-	return (0);
-}
-
