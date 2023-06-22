6d40c4d39d0d0bfab3e49b0b715a5e8878cedab9
root <>
2023-06-22 01:30:39 -0700

    wip task 10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_triangle - draws a triangle in the terminal.
+ *
+ * @n: int
+ *
+ * Return: void.
+ */
+void print_triangle(int n)
+{
+	int i, j;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+
+	for (i = 0; i <= n; i++)
+	{
+		for (j = 0; j < n; j++)
+		{
+			if (j >= n - i)
+				_putchar('#');
+			else
+				_putchar(' ');
+		}
+		if (i != n)
+			_putchar('\n');
+	}
+}
