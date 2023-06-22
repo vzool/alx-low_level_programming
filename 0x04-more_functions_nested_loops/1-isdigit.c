baf87d04de3c688b2ee9f3373911bd7f68475915
root <>
2023-06-21 23:13:21 -0700

    task 1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isdigit - checks for a digit (0 through 9).
+ *
+ * @c: int
+ *
+ * Return: 1 if c is digit, 0 otherwise.
+ */
+int _isdigit(int c)
+{
+	return ((c >= '0' && c <= '9') ? 1 : 0);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,4 +1,5 @@
 #ifndef HEADER_MAIN
 #define HEADER_MAIN
 int _isupper(int c);
+int _isdigit(int c);
 #endif
