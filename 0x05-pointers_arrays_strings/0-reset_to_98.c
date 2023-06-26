54071ae94c8b4fd1deb704a0a0f99b09d90ca6c8
root <>
2023-06-25 22:28:21 -0700

    task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - updates the value it points to bo 98
+ *
+ * @n: int*
+ *
+ * Return: void.
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
+
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,4 @@
+#ifndef HEADER_MAIN
+#define HEADER_MAIN
+void reset_to_98(int *n);
+#endif
