76c1e5dcfce3bec4c7084762e099e34ac9519c1a
root <>
2023-06-25 22:59:24 -0700

    task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string, in reverse, followed by a new line
+ *
+ * @str: char*
+ *
+ * Return: void
+ */
+void print_rev(char *str)
+{
+	int i;
+
+	for (i = 0;; i++)
+		if (str[i] == '\0')
+			break;
+
+	for (; i >= 0; i--)
+		_putchar(str[i]);
+
+	_putchar('\n');
+}
+
