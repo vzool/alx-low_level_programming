19e32e092249c934e55af3c0289bc07c0b9616ce
root <>
2023-06-26 05:29:37 -0700

    Save work

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -10,6 +10,35 @@
  */
 int _atoi(char *s)
 {
-	return (0);
+	int i = 0, size = 0, x = 0, negative = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] >= '0' && s[i] <= '9')
+                {
+			if (i > 0)
+				if (s[i - 1] == '-')
+					negative = 1;
+                }
+		i++;
+	}
+
+	size = i;
+	printf("%s - %d - negative %d\n", s, size, negative);
+
+	for (i = 0; i <= size; i++)
+	{
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			printf("%c\n", s[i]);
+		}
+		i++;
+	}
+
+	if (negative == 1)
+		x *= -1;
+
+	printf("======================================================================\n");
+	return (x);
 }
 
