527fe5eaecd94c5cb8de7a49fef1f58d7b95a227
root <>
2023-06-29 01:28:37 -0700

    task 9

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer.
+ *
+ * @n: int
+ *
+ * Return: void.
+ */
+void print_number(int n)
+{
+	unsigned int x = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		x = n *= -1;
+	}
+	x /= 10;
+	if (x != 0)
+		print_number(x);
+	_putchar('0' + n % 10);
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -9,4 +9,6 @@ char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *s);
 char *rot13(char *);
+int _putchar(char c);
+void print_number(int n);
 #endif
