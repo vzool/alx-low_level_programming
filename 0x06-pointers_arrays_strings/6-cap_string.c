0392d716d050173667b3d0b507855e52262262fe
root <>
2023-06-29 00:44:14 -0700

    task 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes all words of a string.
+ *
+ * @s: char*
+ *
+ * Return: char*
+ */
+char *cap_string(char *s)
+{
+	int i = 0, j, hit;
+	char *match = " -.\n";
+
+	for (; s[i] != '\0'; i++)
+	{
+		hit = 0;
+		if (s[i] >= 'a' && s[i] <= 'z')
+		{
+			if (i == 0)
+				hit = 1;
+			else
+				for (j = 0; match[j] != '\0'; j++)
+					if (s[i - 1] == match[j])
+					{
+						hit = 1;
+						break;
+					}
+
+			if (hit == 1)
+				s[i] += ('A' - 'a');
+		}
+	}
+	return (s);
+}
+
