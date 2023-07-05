802afe715fd1901a6261033c5b90bd540f43bf12
root <>
2023-07-04 22:29:55 -0700

    task 5

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _sqrt - find square root
+ * 
+ * @n: base value
+ * @i: incrementer
+ *
+ * Return: int
+ */
+int _sqrt(int n, int i)
+{
+	if (i * i == n)
+		return (i);
+	if (i * i > n)
+		return (-1);
+	return (_sqrt(n, i + 1));
+}
+
+/**
+ * _sqrt_recursion - square root of a number
+ * 
+ * @n: base value
+ *
+ * Return: int
+ */
+int _sqrt_recursion(int n)
+{
+	int i = 1;
+
+	if (n < 0)
+		return (-1);
+	if (n == 0)
+		return (0);
+	return (_sqrt(n, i));
+}
