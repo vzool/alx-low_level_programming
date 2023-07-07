6194581e586816c5eab967d5f01a0e1c1ad47f7c
Abdelaziz Elrashed <4952736+vzool@users.noreply.github.com>
2023-07-07 08:15:54 +0300

    task 0

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string, followed by a new line, to stdout
+ * @s: string to print
+ *
+ * Return: void
+ */
+void _puts(char *s)
+{
+    while (*s)
+        _putchar(*s++);
+    _putchar('\n');
+}
