02a860d64e2dcabd02177c34da1aff56991dfd34
root <>
2023-07-09 23:29:22 -0700

    task 1 - resolve many issues

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -14,14 +14,16 @@ char *_strdup(char *str)
 	unsigned int i = 0, size = 0;
 	char *s;
 
+	if (str == NULL)
+		return (str);
 	for (; str[size] != '\0'; size++)
 		;
 
 	if (size <= 0)
 		return (NULL);
-	s = malloc(sizeof(char) * size);
+	s = malloc((sizeof(char) * size) + 1);
 	if (s == NULL)
-		return (s);
+		return (NULL);
 	for (; str[i] != '\0'; i++)
 		s[i] = str[i];
 	s[i] = '\0';
