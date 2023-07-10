94b7e36dd5d23eb70122dc8d455d6a7370bc7be7
root <>
2023-07-09 23:14:16 -0700

    don't trust anyone

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
  * create_array - creates an array of chars,
@@ -15,7 +16,14 @@ char *create_array(unsigned int size, char c)
 	unsigned int i = 0;
 	char *s;
 
+	if (size <= 0)
+		printf("failed to allocate memory\n");
 	s = malloc(sizeof(char) * size);
+	if (s == NULL)
+	{
+		printf("failed to allocate memory\n");
+		return (s);
+	}
 	while (i < size)
 		s[i++] = c;
 	s[i] = '\0';
