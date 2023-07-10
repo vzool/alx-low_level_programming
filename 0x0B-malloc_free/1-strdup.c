0bd17f49088042933975a7f70dbf98ab24e27fda
root <>
2023-07-09 23:23:36 -0700

    task 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * _strdup - copy of the string given as a parameter.
+ *
+ * @str: array to duplicate
+ *
+ * Return: pointer to the genererated array
+ */
+char *_strdup(char *str)
+{
+	unsigned int i = 0, size = 0;
+	char *s;
+
+	for (; str[size] != '\0'; size++)
+		;
+
+	if (size <= 0)
+	{
+		printf("failed to allocate memory\n");
+		return (NULL);
+	}
+	s = malloc(sizeof(char) * size);
+	if (s == NULL)
+	{
+		printf("failed to allocate memory\n");
+		return (s);
+	}
+	for (; str[i] != '\0'; i++)
+		s[i] = str[i];
+	s[i] = '\0';
+	return (s);
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,4 +1,5 @@
 #ifndef MAIN_HEADER
 #define MAIN_HEADER
 char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
 #endif
