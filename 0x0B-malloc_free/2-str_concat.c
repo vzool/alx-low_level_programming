0393bc2a1e412c3c57b7256f9cb5916e0e2ba03f
root <>
2023-07-10 00:12:47 -0700

    betty

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -16,9 +16,9 @@ char *str_concat(char *s1, char *s2)
 	char *s;
 
 	if (s1 == NULL)
-                s1 = "";
-        if (s2 == NULL)
-                s2 = "";
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 	for (; s1[size1] != '\0'; size1++)
 		;
 	for (; s2[size2] != '\0'; size2++)
