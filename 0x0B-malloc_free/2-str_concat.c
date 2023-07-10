4717331a68c5f3b727d03acd5f7d97fc549df5c4
root <>
2023-07-10 00:11:07 -0700

    task 2 - reset null to empty strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,10 +13,12 @@
 char *str_concat(char *s1, char *s2)
 {
 	unsigned int i, j, size1 = 0, size2 = 0, size;
-	char *s, *empty = "";
+	char *s;
 
-	if (s1 == NULL && s2 == NULL)
-		return (empty);
+	if (s1 == NULL)
+                s1 = "";
+        if (s2 == NULL)
+                s2 = "";
 	for (; s1[size1] != '\0'; size1++)
 		;
 	for (; s2[size2] != '\0'; size2++)
@@ -25,13 +27,11 @@ char *str_concat(char *s1, char *s2)
 	size = size1 + size2;
 	s = malloc((sizeof(char) * size) + 1);
 	if (s == NULL)
-		return (NULL);
-	if (s1 != NULL)
-		for (i = 0; s1[i] != '\0'; i++)
-			s[i] = s1[i];
-	if (s2 != NULL)
-		for (j = 0; s2[j] != '\0'; j++)
-			s[i++] = s2[j];
+		return (s);
+	for (i = 0; s1[i] != '\0'; i++)
+		s[i] = s1[i];
+	for (j = 0; s2[j] != '\0'; j++)
+		s[i++] = s2[j];
 	s[i] = '\0';
 	return (s);
 }
