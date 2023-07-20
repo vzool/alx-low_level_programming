2290bf61596a85e2e2b74cc0759f02deb0b5e9e0
Abdelaziz Elrashed <4952736+vzool@users.noreply.github.com>
2023-07-20 04:24:46 +0300

    fix issue

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -32,6 +32,12 @@ int main(int argc, char **argv)
 	a = atoi(argv[1]);
 	b = atoi(argv[3]);
 
+	if ((*argv[2] == '/' && b == 0) || (*argv[2] == '%' && b == 0))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
 	result = f(a, b);
 
 	printf("%d\n", result);
