7b63824b560de9ae2c8d11dcd3ab0907af6982ab
root <>
2023-08-07 02:37:10 -0700

    allow to create empty file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,7 +15,7 @@ int create_file(const char *filename, char *text_content)
 {
 	FILE *file;
 
-	if (filename == NULL || text_content == NULL)
+	if (filename == NULL)
 		return (-1);
 
 	file = fopen(filename, "w");
