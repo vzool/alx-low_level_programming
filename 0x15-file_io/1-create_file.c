1ec520cb4d8ab1f2fd67c4e9926d09692cec2104
root <>
2023-08-07 02:34:55 -0700

    fix typo

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -25,7 +25,7 @@ int create_file(const char *filename, char *text_content)
 	if (chmod(filename, 0600) == -1)
 		return (-1);
 
-	if (access(fname, F_OK) == 0)
+	if (access(filename, F_OK) == 0)
 	{
 		if (truncate(filename, 0) == -1)
 			return (-1);
