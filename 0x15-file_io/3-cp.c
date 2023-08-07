d5943b617e62d44c092b041ba0649f5b31e96915
root <>
2023-08-07 03:08:44 -0700

    wip

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
 /**
  * main - copy a file
@@ -16,35 +19,54 @@ int main(int argc, char *argv[])
 
 	if (argc != 3)
 	{
-		printf("Usage: cp file_from file_to\n");
-		exit(EXIT_FAILURE);
+		dprintf(2, "Usage: cp file_from file_to\n");
+		exit(97);
 	}
 
 	fp1 = fopen(argv[1], "r");
-	if (f1 == NULL)
+	if (fp1 == NULL)
 	{
-		printf("Error: Can't read from file %s\n", argv[1]);
-		exit(EXIT_FAILURE);
+		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
 	}
 
 	fp2 = fopen(argv[2], "w");
 	if (fp2 == NULL)
 	{
-		printf("Error: Can't write to %s\n", argv[2]);
-		exit(EXIT_FAILURE);
+		dprintf(2, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+
+	if (access(argv[2], F_OK) == 0)
+	{
+		if (truncate(argv[2], 0) == -1)
+			return (-1);
+	}
+	else
+	{
+		if (chmod(argv[2], 00664) == -1)
+			return (-1);
 	}
 
 	while (fgets(ch, 1024, fp1) != NULL)
 	{
 		if (fputs(ch, fp2) == EOF)
 		{
-			printf("Error: Can't write to %s\n", argv[2]);
+			dprintf(2, "Error: Can't write to %s\n", argv[2]);
 			exit(EXIT_FAILURE);
 		}
 	}
 
-	fclose(fp1);
-	fclose(fp2);
+	if (fclose(fp1) == EOF)
+	{
+		dprintf(2, "Error: Can't close fd\n");
+		exit(100);
+	}
+	if (fclose(fp2) == EOF)
+	{
+		dprintf(2, "Error: Can't close fd\n");
+		exit(100);
+	}
 
 	return (0);
 }
