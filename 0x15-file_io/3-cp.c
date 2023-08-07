e5ba9b9e24d3404ddd65496ae8f4d4bf3975b455
root <>
2023-08-07 03:31:46 -0700

    wip

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -3,6 +3,8 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <fcntl.h>
+#define BUFF_SIZE 1024
 
 /**
  * main - copy a file
@@ -14,58 +16,33 @@
  */
 int main(int argc, char *argv[])
 {
-	FILE *file_from, *file_to;
-	char ch[1024];
+	int file_from, file_to, read_state, write_state;
+	char ch[BUFF_SIZE];
 
 	if (argc != 3)
+		dprintf(2, "Usage: cp file_from file_to\n"), exit(97);
+	file_from = open(argv[1], O_RDONLY);
+	if (file_from == -1)
+		dprintf(2, "Error: Can't read from file %s\n", argv[1]), exit(98);
+	file_to = open(argv[1], O_CREAT | O_WRONLY | O_TRUNC);
+	if (file_to == -1)
+		dprintf(2, "Error: Can't write to %s\n", argv[2]), exit(99);
+	read_state = 1;
+	while (read_state)
 	{
-		dprintf(2, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
-
-	file_to = fopen(argv[2], "w");
-	if (file_to == NULL)
-	{
-		dprintf(2, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
-	}
-
-	if (access(argv[2], F_OK) == 0)
-	{
-		truncate(argv[2], 0);
-	}
-	else
-	{
-		if (chmod(argv[2], 00664) == -1)
-			return (-1);
-	}
-
-	file_from = fopen(argv[1], "r");
-	if (file_from == NULL)
-	{
-		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
-
-	while (fgets(ch, 1024, file_from) != NULL)
-	{
-		if (fputs(ch, file_to) == EOF)
+		read_state = read(file_from, ch, BUFF_SIZE);
+		if (read_state == -1)
+			dprintf(2, "Error: Can't write to %s\n", argv[2]), exit(99);
+		if (read_state > 0)
 		{
-			dprintf(2, "Error: Can't write to %s\n", argv[2]);
-			exit(99);
+			write_state = write(file_to, ch, read_state);
+			if (write_state == -1)
+				dprintf(2, "Error: Can't write to %s\n", argv[2]), exit(99);
 		}
 	}
-
-	if (fclose(file_from) == EOF)
-	{
-		dprintf(2, "Error: Can't close fd\n");
-		exit(100);
-	}
-	if (fclose(file_to) == EOF)
-	{
-		dprintf(2, "Error: Can't close fd\n");
-		exit(100);
-	}
-
+	if (close(file_from) == EOF)
+		dprintf(2, "Error: Can't close fd %d\n", file_from), exit(100);
+	if (close(file_to) == EOF)
+		dprintf(2, "Error: Can't close fd %d\n", file_to), exit(100);
 	return (0);
 }
